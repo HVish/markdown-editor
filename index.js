2e12333f75c9dfdf1c89386fe4fdbbdffca5eebf
Vishnu Singh <vishnusingh1729@gmail.com>
2017-04-14 12:59:26 +0530

    node server edition

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(3001, function () {
+    console.log("Server lifter at http://localhost:3001");
+});
